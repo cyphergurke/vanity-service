63cb4c31c131ceacef963900bcfa743c846a0e75
cyphergurke <cyberzuchini@protonmailcom>
2024-03-24 09:45:50 +0100

    add keypair generator

---
+++ scripts/keypair.ts
@@ -0,0 +1,16 @@
+import * as bitcoin from "bitcoinjs-lib";
+import { ECPairFactory } from 'ecpair';
+import * as ecc from '@bitcoin-js/tiny-secp256k1-asmjs';
+const ECPair = ECPairFactory(ecc);
+
+// const mainnet = bitcoin.networks.bitcoin;
+export const genKeypair = (entropy: string) => {
+    const privateKeyBuffer: Buffer = Buffer.from(entropy);
+    const sha265priv = bitcoin.crypto.sha256(privateKeyBuffer);
+    const keypair = ECPair.fromPrivateKey( sha265priv, { compressed: true, network: bitcoin.networks.bitcoin })
+    const wifkey = keypair.toWIF()
+    const publicKey = keypair.publicKey.toString("hex");
+    console.log(`Private Key (WIF): ${wifkey}`);
+    console.log(`public Key (WIF): ${publicKey}`);
+    return {privKey: wifkey, pubKey: publicKey}
+}
